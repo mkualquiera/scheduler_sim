9ff9986a802e78d08e706eabf2f9314d8fee9e07
Juan Pablo <ozjuanpa@gmail.com>
2023-02-04 18:43:20 -0500

    [ app ] Implemented basic scheduler

--- src/app.rs
+++ src/app.rs
@@ -1,26 +1,87 @@
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+};
+
+use crate::scheduler::{
+    FirstComeFirstServeScheduler, IODeviceState, ProcessState, Scheduler,
+    SchedulerTickOutcome, SchedulerWrapper,
+};
+
+use egui_extras::{Column, TableBuilder};
+use rand::Rng;
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    // this how you opt-out of serialization of a member
+pub struct SchedulerApp {
+    // Mutable reference to the scheduler wrapper.
     #[serde(skip)]
-    value: f32,
+    scheduler_wrapper: Arc<Mutex<SchedulerWrapper<FirstComeFirstServeScheduler>>>,
+
+    #[serde(skip)]
+    last_tick_outcome: Option<SchedulerTickOutcome>,
+
+    // Speed control
+    #[serde(skip)]
+    speed: Arc<Mutex<f32>>,
 }
 
-impl Default for TemplateApp {
+impl Default for SchedulerApp {
     fn default() -> Self {
+        let scheduler_wrapper = Arc::new(Mutex::new(SchedulerWrapper::new(
+            FirstComeFirstServeScheduler::new(),
+        )));
+
+        // Add some processes to the scheduler.
+        let mut scheduler_wrapper_locked = scheduler_wrapper.lock().unwrap();
+        // Create an rng for setting random params for the jobs
+        let mut rng = rand::thread_rng();
+        for _ in 0..15 {
+            let job_duration = rng.gen_range(1..=360);
+            let io_prob = rng.gen_range(0.0..=0.5);
+            scheduler_wrapper_locked
+                .scheduler
+                .register_process(job_duration, io_prob);
+        }
+        drop(scheduler_wrapper_locked);
+
+        let scheduler_wrapper_clone = scheduler_wrapper.clone();
+
+        let speed = Arc::new(Mutex::new(0.0));
+
+        let speed_clone = speed.clone();
+
+        let _ = thread::spawn(move || loop {
+            // Compute sleep duration based on speed
+            // 0 speed, skip iteration
+            // 0.5 speed, sleep 1000 / 30
+            // 1 speed, sleep 1000 / 60
+
+            let curr_speed = *speed_clone.lock().unwrap();
+
+            let sleep_duration = if curr_speed == 0.0 {
+                thread::sleep(std::time::Duration::from_millis(100));
+                continue;
+            } else {
+                (1000.0 / (60.0 * curr_speed)) as u64
+            };
+
+            thread::sleep(std::time::Duration::from_millis(sleep_duration));
+            let mut scheduler_wrapper_clone = scheduler_wrapper_clone.lock().unwrap();
+            scheduler_wrapper_clone.tick();
+            drop(scheduler_wrapper_clone);
+        });
+
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            scheduler_wrapper,
+            last_tick_outcome: None,
+            speed: speed,
         }
     }
 }
 
-impl TemplateApp {
+impl SchedulerApp {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
@@ -36,7 +97,7 @@ impl TemplateApp {
     }
 }
 
-impl eframe::App for TemplateApp {
+impl eframe::App for SchedulerApp {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
@@ -45,13 +106,16 @@ impl eframe::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
+        let Self {
+            scheduler_wrapper, ..
+        } = self;
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
         // Tip: a good default choice is to just keep the `CentralPanel`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
+        /*
         #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
@@ -62,8 +126,9 @@ impl eframe::App for TemplateApp {
                     }
                 });
             });
-        });
+        });*/
 
+        /*
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
             ui.heading("Side Panel");
 
@@ -91,26 +156,171 @@ impl eframe::App for TemplateApp {
                 });
             });
         });
+        */
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
+        egui::CentralPanel::default().show(ctx, |_| {});
 
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
+        // Window that shows the state of processes.
+        egui::Window::new("Process states").show(ctx, |ui| {
+            let scheduler_wrapper = scheduler_wrapper.lock().unwrap();
+
+            let scheduler = &scheduler_wrapper.scheduler;
+            let processes = &scheduler.processes;
+
+            let table = TableBuilder::new(ui)
+                .striped(true)
+                .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+                .column(Column::auto())
+                .column(Column::auto())
+                .column(Column::remainder())
+                .header(20.0, |mut header| {
+                    header.col(|ui| {
+                        ui.strong("PID");
+                    });
+                    header.col(|ui| {
+                        ui.strong("CPU Time");
+                    });
+                    header.col(|ui| {
+                        ui.strong("State");
+                    });
+                });
+
+            table.body(|mut body| {
+                for (i, process) in processes.iter().enumerate() {
+                    // The color will be based on the state of the process.
+                    body.row(30.0, |mut row| {
+                        row.col(|ui| {
+                            ui.label(format!("{:?}", i));
+                        });
+                        row.col(|ui| {
+                            ui.label(format!("{:?}", process.time));
+                        });
+                        row.col(|ui| {
+                            let color = match process.state {
+                                ProcessState::Ready => egui::Color32::YELLOW,
+                                ProcessState::Running => egui::Color32::GREEN,
+                                ProcessState::IOBlocked => egui::Color32::RED,
+                                ProcessState::IORequest => egui::Color32::LIGHT_BLUE,
+                                ProcessState::Finished => egui::Color32::BROWN,
+                            };
+                            ui.colored_label(color, format!("{:?}", process.state));
+                        });
+                    });
+                }
+            });
+            drop(scheduler_wrapper);
+            ctx.request_repaint_after(std::time::Duration::from_millis(1000 / 60));
         });
 
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally choose either panels OR windows.");
-            });
-        }
+        // Window that shows the state of the IO device.
+        egui::Window::new("IO device").show(ctx, |ui| {
+            let scheduler_wrapper = scheduler_wrapper.lock().unwrap();
+            let scheduler = &scheduler_wrapper.scheduler;
+            let io_device = &scheduler.io_device;
+            // The color will be based on the state of the IO device.
+            let color = match io_device.state {
+                IODeviceState::Idle => egui::Color32::YELLOW,
+                IODeviceState::Busy => egui::Color32::GREEN,
+            };
+            ui.colored_label(color, format!("{:?}", io_device.state));
+
+            let progres_bar =
+                egui::ProgressBar::new(io_device.remaining_progress as f32 / 5.0);
+
+            ui.add(progres_bar);
+
+            drop(scheduler_wrapper);
+            ctx.request_repaint_after(std::time::Duration::from_millis(1000 / 60));
+        });
+
+        // Window that shows the scheduler state.
+        egui::Window::new("Scheduler").show(ctx, |ui| {
+            let mut scheduler_wrapper = scheduler_wrapper.lock().unwrap();
+            // Get the last event from the event queue.
+            let event = scheduler_wrapper.event_queue.pop();
+
+            if let Some(event) = event {
+                self.last_tick_outcome = Some(event);
+            }
+
+            if let Some(event) = self.last_tick_outcome {
+                let selected_row: usize = match event {
+                    SchedulerTickOutcome::Idle => 0,
+                    SchedulerTickOutcome::ProcessResumed(_) => 1,
+                    SchedulerTickOutcome::ProcessContinued(_) => 2,
+                    SchedulerTickOutcome::ProcessFinished(_) => 3,
+                    SchedulerTickOutcome::IORequestHandled(_) => 4,
+                    SchedulerTickOutcome::IOFinished(_) => 5,
+                };
+
+                let names = [
+                    "Idle",
+                    "Process resumed",
+                    "Process continued",
+                    "Process finished",
+                    "IO request handled",
+                    "IO finished",
+                ];
+
+                TableBuilder::new(ui)
+                    .striped(true)
+                    .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+                    .column(Column::auto())
+                    .column(Column::auto())
+                    .header(20.0, |mut header| {
+                        header.col(|ui| {
+                            ui.strong("Action");
+                        });
+                        header.col(|ui| {
+                            ui.strong("Active?");
+                        });
+                    })
+                    .body(|mut body| {
+                        for i in 0..6 {
+                            let color = if i == selected_row {
+                                egui::Color32::GREEN
+                            } else {
+                                egui::Color32::WHITE
+                            };
+                            body.row(30.0, |mut row| {
+                                row.col(|ui| {
+                                    ui.colored_label(color, names[i]);
+                                });
+                                row.col(|ui| {
+                                    ui.colored_label(
+                                        color,
+                                        format!("{:?}", i == selected_row),
+                                    );
+                                });
+                            });
+                        }
+                    });
+
+                let used_process: Option<usize> = match event {
+                    SchedulerTickOutcome::IOFinished(process) => Some(process),
+                    SchedulerTickOutcome::ProcessFinished(process) => Some(process),
+                    SchedulerTickOutcome::IORequestHandled(process) => Some(process),
+                    SchedulerTickOutcome::ProcessResumed(process) => Some(process),
+                    SchedulerTickOutcome::ProcessContinued(process) => Some(process),
+                    SchedulerTickOutcome::Idle => None,
+                };
+                // If there is an event, show it.
+                if let Some(process) = used_process {
+                    ui.label(format!("Process: {:?}", process));
+                } else {
+                    ui.label("Idle");
+                }
+            }
+            // Clear event queue for now
+            scheduler_wrapper.event_queue.clear();
+            ctx.request_repaint_after(std::time::Duration::from_millis(1000 / 60));
+        });
+
+        // Window for the simulation speed.
+        egui::Window::new("Simulation speed").show(ctx, |ui| {
+            let mut simulation_speed = *self.speed.lock().unwrap();
+            ui.add(egui::Slider::new(&mut simulation_speed, 0.0..=1.0).text("Speed"));
+            *self.speed.lock().unwrap() = simulation_speed;
+        });
     }
 }
---
+++ src/scheduler.rs
@@ -0,0 +1,276 @@
+use rand::{Rng, SeedableRng};
+
+/// The states in which a process can be.
+#[derive(PartialEq, Copy, Clone, Debug)]
+pub enum ProcessState {
+    Ready,
+    Running,
+    IORequest,
+    IOBlocked,
+    Finished,
+}
+
+/// The possible instructions that a process can return to a scheduler.
+#[derive(PartialEq, Copy, Clone)]
+pub enum ProcessInstruction {
+    /// The process has finished.
+    Finished,
+    /// The process has requested IO.
+    IORequest,
+    /// The process has not finished and has not requested IO.
+    Continue,
+}
+
+/// An entry in the process table.
+pub struct SchedulerProcessEntry {
+    /// The remaining progress of the process (number of time slices it needs to finish).
+    remaining_progress: i32,
+    /// The state of the process.
+    pub state: ProcessState,
+    /// The probability that the process will request IO after finishing a time slice.
+    io_probability: f32,
+    /// The CPU time that the process has used
+    pub time: i32,
+}
+
+impl SchedulerProcessEntry {
+    /// Creates a new process entry.
+    fn new(length: i32, io_probability: f32) -> Self {
+        Self {
+            remaining_progress: length,
+            state: ProcessState::Ready,
+            io_probability,
+            time: 0,
+        }
+    }
+
+    /// Tick the process. This should be called once per time slice.
+    /// Returns the instruction that the scheduler should execute.
+    /// It needs a reference to a rng, because it needs to generate a random number
+    /// to determine whether the process requests IO.
+    fn tick(&mut self, rng: &mut impl Rng) -> ProcessInstruction {
+        match self.state {
+            ProcessState::Running => {
+                self.remaining_progress -= 1;
+                self.time += 1;
+                if self.remaining_progress <= 0 {
+                    self.state = ProcessState::Finished;
+                    ProcessInstruction::Finished
+                } else if rng.gen::<f32>() < self.io_probability {
+                    self.state = ProcessState::IORequest;
+                    ProcessInstruction::IORequest
+                } else {
+                    ProcessInstruction::Continue
+                }
+            }
+            _ => {
+                panic!("Tried to tick a process that was not running.");
+            }
+        }
+    }
+}
+
+#[derive(PartialEq, Copy, Clone, Debug)]
+pub enum IODeviceState {
+    Idle,
+    Busy,
+}
+
+// Implements an IO device.
+pub struct IODevice {
+    /// The state of the IO device.
+    pub state: IODeviceState,
+    /// The number of time slices the IO device needs to finish.
+    pub remaining_progress: i32,
+}
+
+impl IODevice {
+    /// Creates a new IO device.
+    fn new() -> Self {
+        Self {
+            state: IODeviceState::Idle,
+            remaining_progress: 0,
+        }
+    }
+
+    /// Ticks the IO device. This should be called once per time slice.
+    /// Returns the old and new state of the IO device.
+    fn tick(&mut self) -> (IODeviceState, IODeviceState) {
+        let old_state = self.state;
+        if self.state == IODeviceState::Busy {
+            self.remaining_progress -= 1;
+            if self.remaining_progress <= 0 {
+                self.state = IODeviceState::Idle;
+            }
+        }
+        (old_state, self.state)
+    }
+
+    /// Starts the IO device. This should be called when a process requests IO.
+    fn start(&mut self, length: i32) {
+        self.state = IODeviceState::Busy;
+        self.remaining_progress = length;
+    }
+}
+
+/// Possible outcomes of a scheduler tick.
+#[derive(PartialEq, Copy, Clone, Debug)]
+pub enum SchedulerTickOutcome {
+    /// An IO operation has finished, and the process that requested it is now ready.
+    IOFinished(usize),
+    /// An IO request was handled, and the process that requested it is now IOBlocked.
+    IORequestHandled(usize),
+    /// A process has started running.
+    ProcessResumed(usize),
+    /// A process has continued running.
+    ProcessContinued(usize),
+    /// A process has finished.
+    ProcessFinished(usize),
+    /// There is nothing to do.
+    Idle,
+}
+
+/// Trait for a scheduler. A scheduler is responsible for scheduling processes.
+pub trait Scheduler {
+    /// Registers a new process with the scheduler.
+    fn register_process(&mut self, length: i32, io_probability: f32);
+    /// Ticks the scheduler. This should be called once per time slice (or frame,
+    /// when using a graphical interface).
+    fn tick(&mut self) -> SchedulerTickOutcome;
+    /// Create a new scheduler.
+    fn new() -> Self;
+}
+
+/// Implements the first-come-first-serve scheduling algorithm.
+/// This is the simplest scheduling algorithm. It finds a READY process and runs it
+/// until it finishes or requests IO. If a process requests IO, it is moved to the
+/// IOBlocked state, and the scheduler will try to find another process to run in
+/// the meantime.
+pub struct FirstComeFirstServeScheduler {
+    /// The process table.
+    pub processes: Vec<SchedulerProcessEntry>,
+    /// The IO device.
+    pub io_device: IODevice,
+    /// The rng used to determine whether a process requests IO.
+    rng: rand::rngs::StdRng,
+}
+
+impl Scheduler for FirstComeFirstServeScheduler {
+    fn register_process(&mut self, length: i32, io_probability: f32) {
+        self.processes
+            .push(SchedulerProcessEntry::new(length, io_probability));
+    }
+
+    fn tick(&mut self) -> SchedulerTickOutcome {
+        // Tick the IO device.
+        let (old_io_device_state, new_io_device_state) = self.io_device.tick();
+
+        // If the IO device was busy and is now idle, we need to find the process
+        // that is IOBlocked and move it to the Ready state.
+        if old_io_device_state == IODeviceState::Busy
+            && new_io_device_state == IODeviceState::Idle
+        {
+            for (i, process) in &mut self.processes.iter_mut().enumerate() {
+                if process.state == ProcessState::IOBlocked {
+                    process.state = ProcessState::Ready;
+                    return SchedulerTickOutcome::IOFinished(i);
+                }
+            }
+        }
+
+        if new_io_device_state == IODeviceState::Idle {
+            // Now we find a process that has an IORequest, start the IO device and
+            // move the process to the IOBlocked state.
+            for (i, process) in &mut self.processes.iter_mut().enumerate() {
+                if process.state == ProcessState::IORequest {
+                    self.io_device.start(5);
+                    process.state = ProcessState::IOBlocked;
+                    return SchedulerTickOutcome::IORequestHandled(i);
+                }
+            }
+        }
+
+        // Now we find a process that is Running and tick it.
+        for (i, process) in &mut self.processes.iter_mut().enumerate() {
+            if process.state == ProcessState::Running {
+                let instruction = process.tick(&mut self.rng);
+                match instruction {
+                    ProcessInstruction::Finished => {
+                        process.state = ProcessState::Finished;
+                        return SchedulerTickOutcome::ProcessFinished(i);
+                    }
+                    ProcessInstruction::IORequest => {
+                        process.state = ProcessState::IORequest;
+                        return SchedulerTickOutcome::ProcessContinued(i);
+                    }
+                    ProcessInstruction::Continue => {
+                        return SchedulerTickOutcome::ProcessContinued(i);
+                    }
+                }
+            }
+        }
+
+        // Now we find a process that is Ready and start it.
+        for (i, process) in &mut self.processes.iter_mut().enumerate() {
+            if process.state == ProcessState::Ready {
+                process.state = ProcessState::Running;
+                return SchedulerTickOutcome::ProcessResumed(i);
+            }
+        }
+
+        // If we get here, there is nothing to do.
+        SchedulerTickOutcome::Idle
+    }
+
+    fn new() -> Self {
+        Self {
+            processes: Vec::new(),
+            io_device: IODevice::new(),
+            rng: rand::rngs::StdRng::seed_from_u64(0),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_first_come_first_serve_scheduler() {
+        let mut scheduler = FirstComeFirstServeScheduler::new();
+        scheduler.register_process(4, 0.9);
+        scheduler.register_process(3, 0.2);
+        scheduler.register_process(2, 0.1);
+
+        // Iterate it for some ticks and print the outcome of each tick.
+        for _ in 0..100 {
+            let outcome = scheduler.tick();
+            println!("{:?}", outcome);
+        }
+    }
+}
+
+/// A struct that wraps a scheduler and provides an event queue of the events that
+/// the scheduler generates.
+pub struct SchedulerWrapper<S: Scheduler> {
+    /// The scheduler.
+    pub scheduler: S,
+    /// The event queue.
+    pub event_queue: Vec<SchedulerTickOutcome>,
+}
+
+impl<S: Scheduler> SchedulerWrapper<S> {
+    /// Creates a new scheduler wrapper.
+    pub fn new(scheduler: S) -> Self {
+        Self {
+            scheduler,
+            event_queue: Vec::new(),
+        }
+    }
+
+    /// Ticks the scheduler and adds the outcome to the event queue.
+    pub fn tick(&mut self) {
+        let outcome = self.scheduler.tick();
+        self.event_queue.push(outcome);
+    }
+}
